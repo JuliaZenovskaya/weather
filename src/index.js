cccbdea0c4a7e1966cf31bbd263d4ff10ff6d9cc
= <=>
2019-11-07 12:49:02 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+import geoReducer from './reducers/GeoReducer';
+import favReducer from './reducers/FavReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const weatherStore = createStore (combineReducers({
+geo : geoReducer,
+fav : favReducer,
+}), applyMiddleware(thunk));
+
+ReactDOM.render(
+<Provider store = weatherStore>
+  <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
