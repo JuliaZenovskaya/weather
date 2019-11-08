1e3775487fdda15199a96f39d7246ec8ab764ed2
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-11-08 08:57:21 +0300

    last

--- src/localStorage.js
+++
@@ -1,8 +0,0 @@
-export default function getAddedCitiesFromStorage() {
-  const localValue = localStorage.getItem('cities');
-  const localStorageContent = JSON.parse(localValue);
-  let cities = [];
-  if (localStorageContent !== null && Array.isArray(localStorageContent))
-    cities = localStorageContent;
-  return new Map(cities.map(city => [city]));
-}
