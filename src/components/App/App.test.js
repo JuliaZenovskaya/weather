3a4d4bf819b1cd632cd74f0de94c27084dc3b4e0
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-11-19 18:16:26 +0300

    App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const store = configureMockStore([thunk]);
 const response = {
     "coord":{"lon":37.62,"lat":55.75},
     "weather":[{"id":701,"main":"Mist","description":"mist","icon":"50n"}],
@@ -17,5 +18,81 @@ const response = {
     "name":"Moscow",
     "cod":200
   };
+describe("Weather in displaying", () => {
+  it("Weather exists and is not loading", () => {
+    const tempStore = store({
+      geo: {
+            weather: response,
+            isloading: false
+        }
+    });
 
-  it("")
+    const htmlTree = renderer.create(
+      <Provider store={store}>
+        <AddedCities
+          setCoords={()=> {}}
+          setTrue={()=> {}}
+          getWeatherByCoords={()=> {}}
+          getErrorResponse={()=> {}}/>
+     </Provider>).toJSON();
+    expect(htmlTree).toMatchSnapshot();
+  });
+
+  it("Weather exists but is loading", () => {
+    const tempStore = store({
+      geo: {
+            weather: response,
+            isloading: true
+        }
+    });
+
+    const htmlTree = renderer.create(
+      <Provider store={store}>
+        <AddedCities
+          setCoords={()=> {}}
+          setTrue={()=> {}}
+          getWeatherByCoords={()=> {}}
+          getErrorResponse={()=> {}}/>
+     </Provider>).toJSON();
+    expect(htmlTree).toMatchSnapshot();
+  });
+  });
+
+  it("Weather is not exists and is loading", () => {
+    const tempStore = store({
+      geo: {
+            isloading: true
+        }
+    });
+
+    const htmlTree = renderer.create(
+      <Provider store={store}>
+        <AddedCities
+          setCoords={()=> {}}
+          setTrue={()=> {}}
+          getWeatherByCoords={()=> {}}
+          getErrorResponse={()=> {}}/>
+     </Provider>).toJSON();
+    expect(htmlTree).toMatchSnapshot();
+  });
+  });
+
+  it("Some error with displaying weather", () => {
+    const tempStore = store({
+      geo: {
+            error: 'error'
+        }
+    });
+
+    const htmlTree = renderer.create(
+      <Provider store={store}>
+        <AddedCities
+          setCoords={()=> {}}
+          setTrue={()=> {}}
+          getWeatherByCoords={()=> {}}
+          getErrorResponse={()=> {}}/>
+     </Provider>).toJSON();
+    expect(htmlTree).toMatchSnapshot();
+  });
+  });
+});
